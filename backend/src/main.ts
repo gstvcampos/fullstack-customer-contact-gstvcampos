5e2c08ee787467fa8ca2e27835736735d86509b4
gustavo <gustavo.bmc76@gmail.com>
2023-11-27 10:10:24 -0300

    feat: description

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,10 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Contatos de clientes app')
+    .setDescription('Api de consulta contatos dos clientes')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
